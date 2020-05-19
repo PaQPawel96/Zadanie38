30b7840b876d5c0a9bc3d7e9cab8da9ce2a2e1a0
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-19 17:38:24 +0200

    Zadanie38

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-Napisz funkcję, która przyjmuje jako parametry dwie liczby, które przekażesz podczas wywoływania funkcji.
-
-Funkcja ma dodawać, odejmować i mnożyć liczby przekazane do parametrów i wyświetlać w konsoli następujące stringi:
-
-1. "Wynik dodawania wynosi x"
-2. "Wynik odejmowania wynosi x"
-3. "Wynik mnożenia wynosi x"
-
-Gdzie x jest wynikiem danego działania.
-
-Natomiast jeżeli wynikiem jest liczba ujemna, w konsoli powinien pojawić się string:
-"Wynik jest ujemny"
-
 let a=5;
 let b=10;
 function operation(a, b)
